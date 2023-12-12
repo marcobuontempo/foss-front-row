d441df0f0cab0eed6573ea1bf465a828ffb83c0d
marcobuontempo <marco.buon36@gmail.com>
2023-12-12 23:52:53 +1100

    Add CORS policy for backend

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 ### .env
 ```.env
 PORT=<custom_port>
+FRONTEND_URL=<frontend_url_for_cors_access>
 MONGODB_URI=mongodb://<host>:<port>/<collection_name>
 JWT_SECRET_KEY=<custom_signing_key>
+```
+
+## frontend
+### .env
+```.env
+VITE_BACKEND_URL=<backend_base_url>
 ```
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,43 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import mongodb from './utils/database/mongodb';
 import routes from "./routes";
 import errorResponseHandler from '@middlewares/errorResponseHandler.middleware';
-import { authenticate } from '@middlewares/authentication.middleware';
 
-//for .env File 
+// For .env File 
 dotenv.config();
 
+// CORS Options
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+
+// Wrapper for starting server
 async function startServer() {
-    // Init app
-    const app: Application = express();
-    const port = process.env.PORT || 8000;
+  // Init app
+  const app: Application = express();
+  const port = process.env.PORT || 8000;
 
-    // Connect to the database
-    await mongodb.connect();
+  // Connect to the database
+  await mongodb.connect();
 
-    // Pre-Route Middlewares
-    app.use(express.json());                  // Parse JSON in request body
+  // Pre-Route Middlewares
+  app.use(cors(corsOptions));               // Enable CORS for all routes
+  app.use(express.json());                  // Parse JSON in request body
 
-    // Routes
-    app.use(routes);                          // Global routes handler
+  // Routes
+  app.use(routes);                          // Global routes handler
 
-    // Post-Route Middlewares
-    app.use(errorResponseHandler);            // Handles errors and sends appropriate response
+  // Post-Route Middlewares
+  app.use(errorResponseHandler);            // Handles errors and sends appropriate response
 
-    // Start server
-    const server = app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
+  // Start server
+  const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
 }
 
 // Invoke function to start server
