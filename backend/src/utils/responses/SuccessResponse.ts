731e04aba26833a5f7c65f7a18b41044aded0cfd
marcobuontempo <marco.buon36@gmail.com>
2023-12-13 01:24:46 +1100

    Remove unnecessary 'token' field from SuccessResponse class

--- backend/src/utils/responses/SuccessResponse.ts
+++ backend/src/utils/responses/SuccessResponse.ts
@@ -1,19 +1,16 @@
 interface SuccessReponseArgs {
     message?: string;
     data?: Array<any> | object;
-    token?: string;
 }
 
 export class SuccessResponse {
     public success: boolean = true;
     public message?: string = undefined;
     public data?: Array<any> | object = undefined;
-    public token?: string | undefined = undefined;
 
     constructor(args: SuccessReponseArgs) {
         this.message = args.message;
         this.data = args.data;
-        this.token = args.token;
     }
 }
 
