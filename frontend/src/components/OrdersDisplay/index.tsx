790ae2cdbe82d94d11a199b664a04632aa57c53f
marcobuontempo <marco.buon36@gmail.com>
2024-01-16 19:04:47 +1100

    Add user orders display

--- frontend/src/components/OrdersDisplay/index.tsx
+++ frontend/src/components/OrdersDisplay/index.tsx
@@ -1,12 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './OrdersDisplay.css'
+import { UserOrdersResponse, getUserOrders } from '@services/api'
+import { useAppSelector } from '@utils/useAppSelector'
+import { selectUserId } from '@features/auth/authSlice'
 
 type Props = {}
 
-export default function OrdersDisplay({}: Props) {
+export default function OrdersDisplay({ }: Props) {
+  const userid = useAppSelector(selectUserId);
+
+  const [orders, setOrders] = useState<UserOrdersResponse['data']>([]);
+
+  useEffect(() => {
+    if (userid) {
+      getUserOrders(userid)
+        .then(response => {
+          setOrders(response.data);
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }, [])
+
   return (
-    <div>
-      Orders Display
-    </div>
+    <table className='OrdersDisplay table'>
+      <thead>
+        <tr>
+          <th>Order</th>
+          <th>Event Title</th>
+          <th>Ticket</th>
+          <th>Ticket Seat</th>
+          <th>Ticket Price</th>
+          <th>Total Qty</th>
+          <th>Total Price</th>
+          <th>Created</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          orders.map(order => {
+            return order
+            .tickets.map((ticket, ticketIndex) => {
+                const isFirstOrder = ticketIndex === 0;
+                return (
+                  <tr key={ticket._id}>
+                    {
+                      isFirstOrder &&
+                      <>
+                        <td rowSpan={order.totalQuantity}>{order._id}</td>
+                        <td rowSpan={order.totalQuantity}>{order.event.title}</td>
+                      </>
+                    }
+                    <td>{ticket._id}</td>
+                    <td>{ticket.seat}</td>
+                    <td>{ticket.price}</td>
+                    {
+                      isFirstOrder &&
+                      <>
+                        <td rowSpan={order.totalQuantity}>{order.totalQuantity}</td>
+                        <td rowSpan={order.totalQuantity}>{order.totalPrice}</td>
+                        <td rowSpan={order.totalQuantity}>{order.createdAt}</td>
+                      </>
+                    }
+                  </tr>
+                )
+              })
+          })
+        }
+      </tbody>
+    </table>
   )
 }
